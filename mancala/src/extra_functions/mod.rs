89dfdc797aaaaedfea44fc2f83881cb8d5f21f46
David Gawel <DGawel77@gmail.com>
2022-10-03 01:20:04 -0500

    pathing experementation

--- mancala/src/extra_functions/mod.rs
+++ mancala/src/extra_functions/mod.rs
@@ -1,3 +1,9 @@
-pub fn extra_function(){
-    println!("This is the extra_function");
+pub fn open_file(str: String) -> std::io::Result<()> {
+    println!("open_file is in {}", std::env::current_dir()?.display());
+    let result = std::fs::read_to_string(str);
+    match result{
+        Ok(_) => {println!("Found it in open_file!");}
+        Err(_) => {println!("Didnt find it in open_file!");}
+    }
+    Ok(())
 }
