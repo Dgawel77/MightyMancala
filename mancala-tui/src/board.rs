66c51085c6644f66b69da91c6c6c10962aa8644a
David Gawel <DGawel77@gmail.com>
2022-10-23 21:08:59 -0500

    changed the way formatting is done

--- mancala-tui/src/board.rs
+++ mancala-tui/src/board.rs
@@ -1,9 +1,14 @@
+use std::{fmt::format, fs};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 use crate::mancala::{Mancala, GameSettings, GameMode, Difficulty};
 use cursive::{
     event::{Event, EventResult, Key, MouseEvent},
     theme::ColorStyle,
     view::View,
-    Printer, Vec2,
+    Printer, Vec2, XY,
 };
 
 #[derive(Debug)]
@@ -23,70 +28,86 @@ pub struct MancalaBoard {
     //hints: usize,
     //conflict: Option<[usize; 2]>,
     state: BoardState,
+    selected: u8,
     //stopwatch: Stopwatch,
 }
 
+const BOARD_POS_X: usize = 0;
+const BOARD_POS_Y: usize = 0;
 // hold the board info  
 impl MancalaBoard {
+    
     pub fn new() -> Self{
         let settings: GameSettings = GameSettings {mode: GameMode::Capture, difficulty: Difficulty::Random };
         Self {
             mancala: Mancala::new(settings),
             focus:  [0, 0],
-            state: BoardState::Config
+            state: BoardState::Config,
+            selected: 0
         }
     }
 
     fn draw_config(&self, printer: &Printer) {
         printer.print((0, 0), "Press <Enter> to Start!");
-        printer.print((0, 1), "Press <Enter> to Start!");
-        printer.print((0, 2), "Press <Enter> to Start!");
-        printer.print((0, 3), "Press <Enter> to Start!");
-
     }
 
     fn draw_playing(&self, printer: &Printer) {
-        printer.print((0, 0), "╔══════════════════════════════════════════════╗");
-        printer.print((0, 1), "║        ╭───╮╭───╮╭───╮╭───╮╭───╮╭───╮        ║");
-        printer.print((0, 2), &format!("║      ˂ │ {} ││ {} ││ {} ││ {} ││ {} ││ {} │ ˂      ║", self.mancala.game_board[12], self.mancala.game_board[11], self.mancala.game_board[10], self.mancala.game_board[9], self.mancala.game_board[8], self.mancala.game_board[7]));
-        printer.print((0, 3), "║ ╭───╮  ╰───╯╰───╯╰───╯╰───╯╰───╯╰───╯  ╭───╮ ║");
-        printer.print((0, 4), &format!("║ │ {} │    ↓                             │ {} │ ║", self.mancala.game_board[6], self.mancala.game_board[13]));
-        printer.print((0, 5), "║ ╰───╯  ╭╌╌╌╮╭───╮╭───╮╭───╮╭───╮╭───╮  ╰───╯ ║");
-        printer.print((0, 6), &format!("║      ˂ │ {} ││ {} ││ {} ││ {} ││ {} ││ {} │ ˂      ║", self.mancala.game_board[0], self.mancala.game_board[1], self.mancala.game_board[2], self.mancala.game_board[3], self.mancala.game_board[4], self.mancala.game_board[5]));
-        printer.print((0, 7), "║        ╰╌╌╌╯╰───╯╰───╯╰───╯╰───╯╰───╯        ║");
-        printer.print((0, 8), "╚══════════════════════════════════════════════╝");
-        printer.print((0, 9), &format!("{:?}\n  ", self.mancala.game_board));
+        let offset: Printer = printer.offset(XY{x:BOARD_POS_X, y:BOARD_POS_Y});
+        self.draw_base_layer(&offset);
+        self.draw_cells(&offset);
+        self.draw_arrow(&offset);
     }
 
-    fn coord_to_xy(coord: [usize; 2]) -> (usize, usize) {
-        const C: [usize; 9] = [1, 2, 3, 5, 6, 7, 9, 10, 11];
-        (C[coord[1]], C[coord[0]])
-    }
-
-    fn xy_to_coord(xy: (usize, usize)) -> Option<[usize; 2]> {
-        const C: [usize; 13] = [
-            usize::MAX,
-            0,
-            1,
-            2,
-            usize::MAX,
-            3,
-            4,
-            5,
-            usize::MAX,
-            6,
-            7,
-            8,
-            usize::MAX,
-        ];
-        let x = C[xy.0];
-        let y = C[xy.1];
-        if x != usize::MAX && y != usize::MAX {
-            Some([x, y])
-        } else {
-            None
+    fn draw_cells(&self, printer: &Printer){
+        for cell in 0..=13{
+            let (x, y) = MancalaBoard::cell_to_xy(cell);
+            if cell == self.selected {
+                printer.print((x, y), "╭╌╌╌╮");
+                printer.print((x, y+1), &format!("╎ {:<2}╎", self.mancala.game_board[cell as usize]));
+                printer.print((x, y+2), "╰╌╌╌╯");
+            }else{
+                printer.print((x, y), "╭───╮");
+                printer.print((x, y+1), &format!("│ {:<2}│", self.mancala.game_board[cell as usize]));
+                printer.print((x, y+2), "╰───╯");
+            }
         }
     }
+
+    fn draw_arrow(&self, printer: &Printer){
+        let (x, y) = MancalaBoard::cell_to_xy(self.selected);
+        match self.selected {
+            7..=12 => printer.print((x+2,y-1), "↑"),
+            0..=5 => printer.print((x+2,y-1), "↓"),
+            _ => unreachable!(),
+        }
+    }
+
+    fn draw_base_layer(&self, printer: &Printer){
+        if let Ok(lines) = MancalaBoard::read_lines("assets/boardSchematic.txt"){
+            for (position, line) in lines.enumerate(){
+                if let Ok(ip) = line{
+                    printer.print((0, position), &ip);
+                }
+            }
+        }
+    }
+
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+
+    fn cell_to_xy(cell: u8) -> (u8, u8){
+        match cell {
+            6 => (41, 3),
+            13 => (2, 3),
+            0..=5 => (9+cell*5, 5),
+            7..=12 => (69-cell*5, 1),
+            _ => unreachable!()
+        }
+    }
+
 }
 
 // holds how to draw the board on the screen
@@ -101,7 +122,7 @@ impl View for MancalaBoard {
 
     fn required_size(&mut self, _: Vec2) -> Vec2 {
         //  Vec2::new(19, 19)
-        Vec2::new(100, 50)
+        Vec2::new(48, 9)
     }
 
     fn on_event(&mut self, event: Event) -> EventResult {
@@ -115,7 +136,20 @@ impl View for MancalaBoard {
                 }
                 EventResult::Consumed(None)
             }
-            BoardState::Playing => EventResult::Ignored,
+            
+            BoardState::Playing => {
+                match event {
+                    Event::Key(Key::Right) => {
+                        self.selected += 1;
+                    }
+                    Event::Key(Key::Left) => {
+                        self.selected -= 1;
+                    }
+                    _ => return EventResult::Ignored
+                }
+                EventResult::Consumed(None)
+            }
+            
             BoardState::Finish => EventResult::Ignored,
         }
     }
